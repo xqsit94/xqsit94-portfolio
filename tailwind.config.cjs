032158f99fb1a95a0eadbc15efd4ae9aa996df4a
Manikandan B <xqsit94@gmail.com>
2023-08-18 18:42:39 +0530

    dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,16 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        tamil: ['Anek Tamil', 'sans-serif'],
+      },
+      boxShadow: {
+        'dark-3xl': '.05em .3em .4em hsl(234, 41%, 6%)',
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
+            color: theme('colors.gray.300'),
             code: {
               color: theme('colors.pink.500'),
               backgroundColor: theme('colors.pink.100'),
@@ -24,10 +31,22 @@ module.exports = {
       {
         light: {
           ...require('daisyui/src/theming/themes')['[data-theme=light]'],
-          primary: '#22a094',
-          secondary: '#F1C93B',
-          accent: '#FC2947',
+          primary: '#1a725b',
+          secondary: '#F6FA70',
+          accent: '#FF0060',
           neutral: '#0082df',
+        },
+      },
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
+          color: '#d1d5db',
+          'base-100': '#1c1e2b',
+          'base-200': '#373b56',
+          primary: '#20232d',
+          accent: '#F2B807',
+          secondary: '#F24968',
+          neutral: '#23253b',
         },
       },
     ],
