4af7c3e3cc0c47f400d879f2fc10439e50aafb39
Manikandan B <xqsit94@gmail.com>
2023-08-24 22:03:08 +0530

    Implementation of theme using nanostores

--- src/components/blogs/Comments.tsx
+++ src/components/blogs/Comments.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import Giscus, { type GiscusProps } from '@giscus/react';
 import { BLOG } from '~/config.mjs';
+import { $theme } from '~/stores/theme';
+import { useStore } from '@nanostores/react';
 
 const id = 'inject-comments';
 
 const Comments = () => {
   const [mounted, setMounted] = React.useState(false);
+  const theme = useStore($theme);
 
   React.useEffect(() => {
     setMounted(true);
   }, []);
 
   const giscusConfig: GiscusProps = BLOG.giscusConfig as GiscusProps;
+
+  giscusConfig.theme = theme === 'dark' ? 'dark_tritanopia' : 'light_tritanopia';
 
   return (
     <div id={id} className="my-10">
